44a6841b37b37faa2ec45209fdeee4cf38bb7574
James <woghd8503@naver.com>
2022-08-04 12:40:33 +0900

    Pick a queadrant

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Pick_a_quadrant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKJOON_codingTest
+{
+    class Pick_a_quadrant
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int input1 = int.Parse(Console.ReadLine());
+
+            if(input > 0 && input1 > 0)
+                Console.WriteLine("1");
+            if(input < 0 && input1 > 0)
+                Console.WriteLine("2");
+            if(input < 0 && input1 < 0)
+                Console.WriteLine("3");
+            if(input > 0 && input1 < 0)
+                Console.WriteLine("4");
+        }
+    }
+
+}
