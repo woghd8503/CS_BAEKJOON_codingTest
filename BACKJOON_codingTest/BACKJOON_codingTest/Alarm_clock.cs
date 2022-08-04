466cfdcd36d04cc0cc899b7b71ac22ad54b23b7c
James <woghd8503@naver.com>
2022-08-04 13:50:51 +0900

    Alarm clock

--- BACKJOON_codingTest/BACKJOON_codingTest/Alarm_clock.cs
+++ BACKJOON_codingTest/BACKJOON_codingTest/Alarm_clock.cs
@@ -8,5 +8,29 @@ namespace BACKJOON_codingTest
 {
     class Alarm_clock
     {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int hour = int.Parse(input[0]);
+            int minute = int.Parse(input[1]);
+
+            if (hour < 0 || hour > 24 || minute < 0 || minute > 59) return;
+
+            if (minute < 45)
+            {
+                hour--;
+                minute += 15;
+
+                if (hour < 0)
+                    hour = 23;
+            }
+            else
+            {
+                minute -= 45;
+            }
+
+            Console.WriteLine(hour + " " + minute);
+        }
     }
+
 }
