382d520909b68ceab070b77378d9f9c7d4603b76
James <woghd8503@naver.com>
2022-08-04 12:00:28 +0900

    BackJoon codingTest

---
+++ ConsoleApp1/ConsoleApp1/Pick_a_quadrant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CondingTest
+{
+    class Pick_a_quadrant
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
