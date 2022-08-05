f7e265ae2b7c87fd2254c53b362a587cd8af00c7
James park <woghd8503@naver.com>
2022-08-05 19:22:47 +0900

    A_B - 3

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/A+B-3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKJOON_codingTest
+{
+    class A_B_3
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] answer = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                string[] inputNums = Console.ReadLine().Split();
+                int num1 = int.Parse(inputNums[0]);
+                int num2 = int.Parse(inputNums[1]);
+                answer[i] = num1 + num2;
+            }
+
+            for(int i = 0; i < input; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+        }
+    }
+
+}
