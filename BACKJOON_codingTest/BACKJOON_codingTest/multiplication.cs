696b2cdf50cfbba79b75cf4209ddb26172cce3b4
James park <woghd8503@naver.com>
2022-08-05 19:05:55 +0900

    Multiplication

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/multiplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKJOON_codingTest
+{
+    class multiplication
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"{input} * {i} = {i*input}");
+            }
+        }
+    }
+
+}
