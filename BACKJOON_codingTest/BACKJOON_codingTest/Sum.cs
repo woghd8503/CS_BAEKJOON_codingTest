434a47bc0c6e31a670fea2703957b1a12e31f5fe
James park <woghd8503@naver.com>
2022-08-06 10:20:33 +0900

    Sum

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKJOON_codingTest
+{
+    class Sum
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= input; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
+}
