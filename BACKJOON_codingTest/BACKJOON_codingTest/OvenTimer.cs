b3ac01d9a6b96b362e418e3f6ce7561147f65ecf
James park <woghd8503@naver.com>
2022-08-06 11:05:49 +0900

    OvenTimer

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/OvenTimer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class OvenTimer
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int hour = int.Parse(input[0]);
+            int minute = int.Parse(Console.ReadLine()) + int.Parse(input[1]);
+
+            hour = (hour + minute / 60) % 24;
+            minute %= 60;
+
+            Console.WriteLine(hour + " " + minute);
+        }
+    }
+
+}
