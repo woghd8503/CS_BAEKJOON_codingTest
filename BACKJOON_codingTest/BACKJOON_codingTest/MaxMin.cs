e8a8fd0c64e9d26955d4acab248d349332c5fa45
James park <woghd8503@naver.com>
2022-08-07 09:38:33 +0900

    MaxMin

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/MaxMin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class MaxMin
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            string[] input1 = Console.ReadLine().Split();
+            int[] inputArray = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                inputArray[i] = int.Parse(input1[i]);
+            }
+
+            Array.Sort(inputArray);
+
+
+            Console.WriteLine(inputArray[0] + " " + inputArray[input -1]);
+        }
+    }
+
+}
