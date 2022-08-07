0bc5f5f334e67f5805e940664d34959a7fb69fa0
James park <woghd8503@naver.com>
2022-08-07 14:51:27 +0900

    MaxValue

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/MaxValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class MaxValue
+    {
+        static void Main()
+        {
+            int totalCount = 9;
+            int[] numbers = new int[totalCount];
+            int maxValue = 0;
+            int count = 0;
+
+            for(int i = 0; i < totalCount; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < totalCount; i++)
+            {
+                if(maxValue < numbers[i])
+                {
+                    maxValue = numbers[i];
+                    count = i + 1;
+                }
+            }
+            Console.WriteLine(maxValue);
+            Console.WriteLine(count);
+        }
+    }
+
+}
