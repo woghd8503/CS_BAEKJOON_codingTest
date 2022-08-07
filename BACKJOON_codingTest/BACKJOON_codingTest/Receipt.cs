64123e9608798c9738a6aa72aff989b4118254c3
James park <woghd8503@naver.com>
2022-08-07 14:26:52 +0900

    Receipt

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Receipt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class Receipt
+    {
+        static void Main()
+        {
+            int totalPayment = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            int sumMoney = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] priceAndNum = Console.ReadLine().Split();
+                sumMoney += int.Parse(priceAndNum[0]) * int.Parse(priceAndNum[1]);
+            }
+
+            if(sumMoney == totalPayment)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+
+}
