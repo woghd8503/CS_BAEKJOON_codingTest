433957b57a58d3d03d50315d4e3c02cd36c91ee4
James park <woghd8503@naver.com>
2022-08-07 09:48:59 +0900

    Chess

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Chess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class Chess
+    {
+        static void Main()
+        {
+            int[] chessNum = new int[6] { 1, 1, 2, 2, 2, 8 };
+
+            string[] input = Console.ReadLine().Split();
+
+            for (int i = 0; i < input.Length; i++)
+                chessNum[i] -= int.Parse(input[i]);
+
+            for (int i = 0; i < input.Length; i++)
+                Console.Write(chessNum[i] + " ");
+        }
+    }
+
+}
