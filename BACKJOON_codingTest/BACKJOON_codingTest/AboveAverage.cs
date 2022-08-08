b6cb31c2cdb1cdc3ec35d8748e5c25cbc0ae8d85
James park <woghd8503@naver.com>
2022-08-09 00:21:25 +0900

    AboveAverage

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/AboveAverage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKJOON_codingTest
+{
+    class AboveAverage
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            float average = 0.0f;
+            int result = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int sum = 0;
+                result = 0;
+
+                string[] input = Console.ReadLine().Split();
+
+                for(int j = 1; j < input.Length; j++)
+                {
+                    sum += int.Parse(input[j]);
+                }
+                average = sum / int.Parse(input[0]);
+
+                for(int j = 1; j < input.Length; j++)
+                {
+                    if (int.Parse(input[j]) > average)
+                        result++;
+                }
+                Console.WriteLine($"{(float)result / float.Parse(input[0]) * 100:0.000}%");
+            }
+        }
+    }
+
+}
