ebce19e006c0c367d8d1085fe929827c360d3133
James <woghd8503@naver.com>
2022-08-08 19:59:01 +0900

    PlusCycle

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/PlusCycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class PlusCycle
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int firstNum = input;
+            int answer = 0;
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            while (true)
+            {
+                num1 = input / 10;
+                num2 = input % 10;
+                num3 = (num1 + num2) % 10;
+                input = num3 + (num2 * 10);
+                answer++;
+                if (input == firstNum)
+                    break;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+
+}
