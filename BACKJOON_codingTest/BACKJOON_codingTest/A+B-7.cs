0745b7b73c843d0c8c10f406950b246b64a16101
James <woghd8503@naver.com>
2022-08-08 15:35:20 +0900

    A+B-7

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/A+B-7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace BACKJOON_codingTest
+{
+    class A_B_7
+    {
+        static void Main()
+        {
+            StringBuilder number = new StringBuilder();
+
+            int count = int.Parse(Console.ReadLine());
+            int[] num = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                String[] input = Console.ReadLine().Split();
+                num[i] = int.Parse(input[0]) + int.Parse(input[1]);
+                number.AppendLine($"Case #{i + 1}: " + num[i].ToString());
+            }
+
+            Console.WriteLine(number.ToString());
+        }
+    }
+
+}
+
+
