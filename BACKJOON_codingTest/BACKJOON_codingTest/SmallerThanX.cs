6d927d3eb5ddfebc9d2544931c868b3b0d89e15b
James <woghd8503@naver.com>
2022-08-08 10:28:11 +0900

    Smaller than X

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/SmallerThanX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class SmallerThanX
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int count = int.Parse(input[0]);
+            int standardNum = int.Parse(input[1]);
+            string[] input1 = Console.ReadLine().Split();
+
+            for(int i = 0; i < count; i++)
+            {
+                if (int.Parse(input1[i]) >= standardNum)
+                    continue;
+                else
+                    Console.Write(input1[i] + " ");
+            }
+        }
+    }
+
+}
