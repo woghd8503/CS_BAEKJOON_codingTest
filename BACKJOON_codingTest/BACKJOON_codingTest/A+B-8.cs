f0d762691f6a576db64d25734287791522153a4c
James <woghd8503@naver.com>
2022-08-08 15:16:08 +0900

    A+B-8

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/A+B-8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class A_B_8
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                String[] input = Console.ReadLine().Split();
+
+                Console.WriteLine($"Case #{i + 1}: {int.Parse(input[0])} + {int.Parse(input[1])} = {int.Parse(input[0]) + int.Parse(input[1])}");
+            }
+        }
+    }
+
+}
