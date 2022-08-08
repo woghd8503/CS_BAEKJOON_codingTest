794738fa6957fb8cdda4285665d71c1c658ba613
James <woghd8503@naver.com>
2022-08-08 16:14:44 +0900

    A+B-fast

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/A+B-fast.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+
+namespace BACKJOON_codingTest
+{
+    class A_B_fast
+    {
+        static void Main()
+        {
+            StringBuilder answer = new StringBuilder();
+            int count = int.Parse(Console.ReadLine());
+
+            int[] a = new int[count];
+            int[] b = new int[count];
+
+            for(int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                a[i] = int.Parse(input[0]);
+                b[i] = int.Parse(input[1]);
+
+                int sum = a[i] + b[i];
+                answer.AppendLine(sum.ToString());
+            }
+
+            Console.WriteLine(answer.ToString());
+        }
+    }
+
+}
