2cc82e67be1ad7147847a2bdf9adbbc86baa0ef2
James <woghd8503@naver.com>
2022-08-08 10:48:34 +0900

    A+B-5

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/A+B-5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BACKJOON_codingTest
+{
+    class A_B_5
+    {
+        static void Main()
+        {
+            List<int> answer = new List<int>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                int num1 = int.Parse(input[0]);
+                int num2 = int.Parse(input[1]);
+
+                if (num1 + num2 == 0)
+                    break;
+
+                answer.Add(num1 + num2);
+            }
+
+            for (int i = 0; i < answer.Count; i++)
+                Console.WriteLine(answer[i]);
+        }
+    }
+
+}
