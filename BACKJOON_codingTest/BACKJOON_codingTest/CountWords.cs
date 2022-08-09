ff11cf0dcf3bc3b466d85bb3c0c4335f6491bef1
James park <woghd8503@naver.com>
2022-08-09 18:11:37 +0900

    CountWords

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/CountWords.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class CountWords
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine().Trim();
+            int answer = 0;
+            
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                    answer++;
+            }
+            if(input.Length == 0)
+                Console.WriteLine(0);
+            else
+                Console.WriteLine(answer + 1);
+        }
+    }
+
+}
