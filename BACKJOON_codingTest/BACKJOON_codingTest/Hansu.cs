42298b7cd6f66abb4af19173a3fbee7200b33bab
James park <woghd8503@naver.com>
2022-08-09 17:31:57 +0900

    Hansu

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Hansu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class Hansu
+    {
+        static void Main()
+        {
+            int count = 0;
+            int input = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= input; i++)
+            {
+                if (Method(i.ToString()))
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static bool Method(string num)
+        {
+            if(int.Parse(num) < 100)
+            return true;
+
+            int[] numbers = new int[num.Length];
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                numbers[i] = num[i];
+            }
+
+            if ((numbers[0] - numbers[1]) == numbers[1] - numbers[2])
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+}
