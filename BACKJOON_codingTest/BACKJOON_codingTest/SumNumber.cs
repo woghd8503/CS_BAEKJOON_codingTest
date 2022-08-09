370b300cdd1b8f9fff0c8f32eeb23cf967acb365
James park <woghd8503@naver.com>
2022-08-09 17:37:20 +0900

    SumNumber

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/SumNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class SumNumber
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            char[] arrChar = Console.ReadLine().ToCharArray();
+            int answer = 0;
+
+            for(int i = 0; i < arrChar.Length; i++)
+            {
+                answer += (int)char.GetNumericValue(arrChar[i]);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+
+}
