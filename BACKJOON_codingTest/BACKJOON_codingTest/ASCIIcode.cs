194d93e98cad6cfb153782b7c36cdbf69b60605c
James park <woghd8503@naver.com>
2022-08-09 17:31:47 +0900

    ASCII code

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/ASCIIcode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class ASCIIcode
+    {
+        static void Main()
+        {
+            char input = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine((int)input);
+        }
+    }
+
+}
