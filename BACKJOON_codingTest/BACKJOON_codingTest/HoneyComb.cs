85f6ea30aed4fea5344dfd5a077a1f031789454c
James park <woghd8503@naver.com>
2022-08-11 14:30:35 +0900

    HoneyComb

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/HoneyComb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class HoneyComb
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int cnt = 1; 
+            long range = 1; 
+            long temp = 1; 
+
+            while (true)
+            {
+                if (range >= input) 
+                {
+                    break;
+                }
+                temp = 6 * cnt++; 
+                range += temp; 
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+
+}
