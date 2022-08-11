2879b274d5662f953389e1fe8a6e41f868ae1aba
James park <woghd8503@naver.com>
2022-08-11 14:26:23 +0900

    Const

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Const.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class Const
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int[] a = Array.ConvertAll(s, int.Parse);
+
+            int num1 = 0;
+            int num2 = 0;
+
+            while (a[0] > 0)
+            {
+                num1 += a[0] % 10;
+                num1 *= 10;
+                a[0] /= 10;
+            }
+            while (a[1] > 0)
+            {
+                num2 += a[1] % 10;
+                num2 *= 10;
+                a[1] /= 10;
+            }
+            Console.WriteLine(num1 >= num2 ? num1 / 10 : num2 / 10);
+        }
+    }
+
+}
