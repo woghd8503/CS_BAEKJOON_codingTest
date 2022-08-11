213f9d78d9049b9ee3fc0c82234956509e8b0ed4
James park <woghd8503@naver.com>
2022-08-11 14:30:12 +0900

    FindAlphabet

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/FindAlphabet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace BACKJOON_codingTest
+{
+    class FindAlphabet
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 97; i < 123; i++)
+            {
+                if (s.Contains(Convert.ToChar(i)))
+                {
+                    Console.Write(s.IndexOf(Convert.ToChar(i)));
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("-1 ");
+                }
+            }
+        }
+    }
+
+}
+
