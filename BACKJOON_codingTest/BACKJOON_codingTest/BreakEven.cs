4ae1d0694df443c4730ac89f54add20b3edfa401
James park <woghd8503@naver.com>
2022-08-11 14:29:40 +0900

    BreakEven

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/BreakEven.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class BreakEven
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+
+            if (B >= C)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+            else
+            {
+                int n = A / (C - B);
+                Console.WriteLine(n + 1);
+            }
+        }
+    }
+
+}
