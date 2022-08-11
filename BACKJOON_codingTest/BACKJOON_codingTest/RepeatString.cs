45cbb214b10c3458219f1edea2577f35e58e26f4
James park <woghd8503@naver.com>
2022-08-11 14:26:08 +0900

    RepeatString

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/RepeatString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class RepeatString
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string answer = "";
+                int t = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < t; i++)
+                {
+                    string[] s = Console.ReadLine().Split();
+                    int r = int.Parse(s[0]);
+
+                    for (int j = 0; j < s[1].Length; j++)
+                    {
+                        for (int k = 0; k < r; k++)
+                        {
+                            answer += s[1][j];
+                        }
+                    }
+                    Console.WriteLine(answer);
+                    answer = "";
+                }
+            }
+        }
+    }
+
+}
