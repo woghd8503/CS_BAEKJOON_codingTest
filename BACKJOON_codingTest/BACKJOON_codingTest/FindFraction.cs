43cacde5461814abcf10ea9661e99c35a3ed4241
James park <woghd8503@naver.com>
2022-08-12 22:09:06 +0900

    FindFraction

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/FindFraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class FindFraction
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int level = 1;
+            int temp = 1;
+
+            while(temp < input)
+            {
+                level++;
+                temp = temp + level;
+            }
+
+            int den = temp - input + 1;
+
+            if (level % 2 == 0)
+                Console.WriteLine((level + 1 - den) + "/" + den);
+            else
+                Console.WriteLine(den + "/" + (level + 1 - den));
+        }
+    }
+
+}
