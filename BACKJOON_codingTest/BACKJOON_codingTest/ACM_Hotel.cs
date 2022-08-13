edd2036469f0beb52a8aa5e62809f6e772e0785b
James park <woghd8503@naver.com>
2022-08-13 23:36:45 +0900

    ACM hotel

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/ACM_Hotel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class ACM_Hotel
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < input; i++)
+            {
+                string[] testCase = Console.ReadLine().Split();
+                int h = int.Parse(testCase[0]);
+                int w = int.Parse(testCase[1]);
+                int n = int.Parse(testCase[2]);
+                int x = 1, y = 1;
+
+                while(true)
+                {
+                    if(n > h)
+                    {
+                        n -= h;
+                        y++;
+                    }
+                    else
+                    {
+                        x = n;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0}{1:00}", x, y);
+            }
+        }
+    }
+
+}
