ac7a0041e995ddf3c81a62ce36a31ddb1ea7c254
James <woghd8503@naver.com>
2022-08-17 15:35:14 +0900

    Factorial

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class Factorial
+    {
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialMethod(n));
+        }
+
+        static int FactorialMethod(int num)
+        {
+            int result = num;
+            for (int i = 1; i < num; i++)
+            {
+                result = result * (num - i);
+            }
+
+            if (result == 0) result = 1;
+
+            return result;
+        }
+    }
+
+}
