6f731680e73085180b227abf47f5ddeaf9c9c483
James park <woghd8503@naver.com>
2022-08-19 00:01:27 +0900

    DeliverSugar

---
+++ BACKJOON_codingTest/BACKJOON_codingTest/DeliverSugar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BACKJOON_codingTest
+{
+    class DeliverSugar
+    {
+        static void Main()
+        {
+
+            int input = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            while (input > 0)
+            {
+                if (input % 5 == 0)
+                {
+                    input -= 5;
+                    count++;
+                }
+                else if (input % 3 == 0)
+                {
+                    input -= 3;
+                    count++;
+                }
+                else if (input > 5)
+                {
+                    input -= 5;
+                    count++;
+                }
+                else
+                {
+                    count = -1;
+                    break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+
+}
